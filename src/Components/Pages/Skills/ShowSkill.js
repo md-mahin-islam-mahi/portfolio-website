6ec4f4fa88b887ee8ee6a7e8a00ad4bb5ac8b744
md-mahin-islam-mahi <wdmahin.mhm@gmail.com>
2023-11-29 21:26:52 +0600

    updated the skills component

--- src/Components/Pages/Skills/ShowSkill.js
+++ src/Components/Pages/Skills/ShowSkill.js
@@ -5,7 +5,7 @@ import { RiStarSFill } from "react-icons/ri";
 const ShowSkill = ({skill}) => {
     const {name, img, stars, skillRate} = skill;
     return (
-        <div className='hover down flex flex-col justify-center items-center px-3 py-2 my-0 mx-auto mb-5 mt-20 bg-mainBg border-2 border-mainFont rounded-lg w-[200px] h-[250px] gap-3 relative overflow-hidden z-0'>
+        <div className='hover down flex flex-col justify-center items-center px-3 py-2 my-0 mx-auto mb-5 mt-20 bg-mainBg border-2 border-mainFont rounded-lg w-[170px] h-[220px] gap-3 relative overflow-hidden z-0'>
             <img className='h-[80px]' src={img} alt={name} />
             <h2 className='text-[30px] font-bold'>{name}</h2>
             <div className='card-items font-bold text-[30px] tracking-widest'>
