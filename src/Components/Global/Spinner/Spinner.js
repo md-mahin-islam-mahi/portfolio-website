89bb5c456162f7b7336497e39a4060f216cb333f
md-mahin-islam-mahi <wdmahin.mhm@gmail.com>
2023-12-03 17:27:05 +0600

    updated

--- src/Components/Global/Spinner/Spinner.js
+++ src/Components/Global/Spinner/Spinner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Spinner = () => {
     return (
-        <div className='flex flex-col justify-center items-center mt-[400px]'>
+        <div className='flex flex-col justify-center items-center mt-[250px] lg:mt-[400px]'>
             <div className='h-10 w-10 border-mainFont border-[5px] rounded-xl animate-spin mb-3'>
             </div>
             <h2 className='text-mainFont text-[35px]'>Loading from the server...</h2>
